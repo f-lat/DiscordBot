a77c031d622d5441b4106d90c387afec0653fc6a
f-lat <l181127@lhr.nu.edu.pk>
2020-07-13 21:46:33 +0500

    final

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js');
 const token = 'NzMyMjYxMzk1ODkwMDQ0OTM2.XwyHPg.Te-8eGOGtU8yTehYUomHt8YP3ao';
 const bot = new Discord.Client();
 
-
-bot.login(token);
-
-bot.on('ready', () => {
-	console.info('Logged in as ${bot.user.tag}!');
+bot.on('ready', () => {
+	console.info('Bot is now connected!');
 	});
 
 bot.on('message',(msg)=> {
-	if (msg.content ==='ping') 
+	if (msg.author.discriminator ==='5187') 
 	{
-		msg.channel.send('pong');
-		msg.channel.send(msg.author);
+		msg.channel.send('han bharway');
 	}	
 	});
+
+	
+
+bot.login(token);
