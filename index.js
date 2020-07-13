7d9f3f361335ed9ed18c8fb0be5bcd23a65f2e61
f-lat <l181127@lhr.nu.edu.pk>
2020-07-13 21:50:40 +0500

    'final'

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ bot.on('ready', () => {
 	});
 
 bot.on('message',(msg)=> {
-	if (msg.author.discriminator ==='5187') 
+	if (msg.author.discriminator ==='6629' || msg.author.username === 'Naveed Ali') 
 	{
-		msg.channel.send('han bharway');
+		msg.channel.send('Naveed ney chuss mari hai bhai sun lo !');
 	}	
 	});
 
-	
+
 
 bot.login(token);
